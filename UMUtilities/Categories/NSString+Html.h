7e7de2219e6d60430713ddc329c1c85b17671f89
Ramon Vicente <ramonvicentesilva@hotmail.com>
2015-12-06 08:55:32 -0200

    Dealing with NSStrings that contain HTML Utilitary based on google-toolbox-form-mac

---
+++ UMUtilities/Categories/NSString+Html.h
@@ -0,0 +1,46 @@
+//
+//  NSString+Html.h
+//  OscarParty
+//
+//  Created by Ramon Vicente on 3/25/15.
+//  Copyright (c) 2015 UMOBI. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface NSString (Html)
+
+-(BOOL)isHtml;
+
+// Strips HTML tags & comments, removes extra whitespace and decodes HTML character entities.
+- (NSString *)stringByConvertingHTMLToPlainText;
+
+// Decode all HTML entities using GTM.
+- (NSString *)stringByDecodingHTMLEntities;
+
+// Encode all HTML entities using GTM.
+- (NSString *)stringByEncodingHTMLEntities;
+
+// Minimal unicode encoding will only cover characters from table
+// A.2.2 of http://www.w3.org/TR/xhtml1/dtds.html#a_dtd_Special_characters
+// which is what you want for a unicode encoded webpage.
+- (NSString *)stringByEncodingHTMLEntities:(BOOL)isUnicode;
+
+// Replace newlines with <br /> tags.
+- (NSString *)stringWithNewLinesAsBRs;
+
+// Remove newlines and white space from string.
+- (NSString *)stringByRemovingNewLinesAndWhitespace;
+
+// Wrap plain URLs in <a href="..." class="linkified">...</a>
+//  - Ignores URLs inside tags (any URL beginning with =")
+//  - HTTP & HTTPS schemes only
+//  - Only works in iOS 4+ as we use NSRegularExpression (returns self if not supported so be careful with NSMutableStrings)
+//  - Expression: (?<!=")\b((http|https):\/\/[\w\-_]+(\.[\w\-_]+)+([\w\-\.,@?^=%&amp;:/~\+#]*[\w\-\@?^=%&amp;/~\+#])?)
+//  - Adapted from http://regexlib.com/REDetails.aspx?regexp_id=96
+- (NSString *)stringByLinkifyingURLs;
+
+- (NSAttributedString *) attributedString;
+
+@end
